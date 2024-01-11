3c2c332216d507ec12efbbf5b20d98bb81d21f34
Sunny Gulati <sunnygulati@kenmarkeyewear.com>
2024-01-11 01:20:24 -0500

    Rewriting (again)
    WPF .. reading emails from gmail.. its very slow :(  and no UI update while its going.

---
+++ GmailFilterLibrary/Class1.cs
@@ -0,0 +1,70 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+namespace GmailFilterLibrary
+{
+    public class GmailFilter1
+    {
+        static string[] Scopes = { GmailService.Scope.GmailReadonly };
+        static string ApplicationName = "Sunny Gmail 2024 Filter";
+        private GmailService _gmailService;
+
+        public void Connect(string credentialFile, string credPath)
+        {
+            UserCredential credential;
+            using (var stream =
+                   new FileStream(credentialFile, FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+            _gmailService = new GmailService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+        }
+
+        public List<Message> Emails { get; set; }
+        
+        public Action<string> Log { get; set; }
+        
+        public void LoadEmails(int numDaysToLoad)
+        {
+            Emails = new List<Message>();
+         // load several batches of emails from gmail inbox "me" until we get older than 30 days ago
+         var currentDate = DateTime.Now;
+            var oldestDate = currentDate.AddDays(-numDaysToLoad);
+            var request = _gmailService.Users.Messages.List("me");
+            request.Q = $"after:{oldestDate.ToString("yyyy/MM/dd")}";
+            request.MaxResults = 100;
+            do
+            {
+                var response = request.Execute();
+                var messages = response.Messages;
+                if (messages != null)
+                {
+                    foreach (var message in messages)
+                    {
+                        var email = _gmailService.Users.Messages.Get("me", message.Id).Execute();
+                        if (email != null)
+                        {
+                            Emails.Add(email); 
+                        }
+                    }
+                    Log?.Invoke($"Loaded {Emails.Count} emails so far");
+                }
+                request.PageToken = response.NextPageToken;
+            } while (!string.IsNullOrEmpty(request.PageToken));
+         
+        }
+    }
+}
---
+++ GmailFilterWpf/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GmailFilterLibrary;
+
+namespace GmailFilterWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GmailFilter1 _gmf;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                StatusText.Text = "Connecting...";
+                StatusText.Foreground = Brushes.Black;
+                _gmf = new GmailFilter1();
+
+                _gmf.Log = (m) =>
+                {
+                    Trace.WriteLine(m);
+                };
+                
+                _gmf.Connect(CredentialsFileText.Text, TokenFileText.Text);
+                StatusText.Text = "Connected.";
+                StatusText.Foreground = Brushes.Blue;
+
+                int numDaysToLoad = int.Parse(DaysToLoadText.Text);
+
+                _gmf.LoadEmails(numDaysToLoad);
+                _gmf.Log("Done Loading");
+            }
+            catch (Exception ex)
+            {
+                StatusText.Foreground = Brushes.Red; 
+                StatusText.Text = ex.Message; 
+            }
+        }
+    }
+}
