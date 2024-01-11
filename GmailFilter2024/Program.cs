60c1e704dcdcd42b506dc5d9263dd0b4e477b478
Sunny Gulati <sunnygulati@kenmarkeyewear.com>
2024-01-10 19:57:37 -0500

    Yet again, my attempt to use .net core has failed.  outlook interop.
    This code finds the Inbox which i need to do some work on

--- GmailFilter2024/Program.cs
+++ GmailFilter2024/Program.cs
@@ -3,9 +3,8 @@ using Google.Apis.Gmail.v1;
 using Google.Apis.Gmail.v1.Data;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
-using System;
-using System.IO;
-using System.Threading;
+
+namespace GmailFilter2024;
 
 public class GmailAPI
 {
@@ -18,9 +17,10 @@ public class GmailAPI
     static string ApplicationName = "Sunny Gmail 2024 Filter";
     public static void Main(string[] args)
     {
+        /*
         UserCredential credential;
         using (var stream =
-            new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+               new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
         {
             string credPath = "token.json";
             credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
@@ -36,32 +36,33 @@ public class GmailAPI
             HttpClientInitializer = credential,
             ApplicationName = ApplicationName,
         });
+        */
         /*
-        var labelListRequest = service.Users.Labels.List("me");
-        var labelListResponse = labelListRequest.Execute();
-        if (labelListResponse == null) throw new NullReferenceException("Got back a null list of labels");
-        foreach (var label in labelListResponse.Labels)
-        {
-            Console.WriteLine($"Label: {label.Id} {label.Name} {label.ETag} {label.LabelListVisibility} {label.MessageListVisibility} {label.MessagesTotal} {label.ThreadsTotal} {label.Type}");
-        }
-        */
+    var labelListRequest = service.Users.Labels.List("me");
+    var labelListResponse = labelListRequest.Execute();
+    if (labelListResponse == null) throw new NullReferenceException("Got back a null list of labels");
+    foreach (var label in labelListResponse.Labels)
+    {
+        Console.WriteLine($"Label: {label.Id} {label.Name} {label.ETag} {label.LabelListVisibility} {label.MessageListVisibility} {label.MessagesTotal} {label.ThreadsTotal} {label.Type}");
+    }
+    */
         /*        UsersResource.MessagesResource.ListRequest request = service.Users.Messages.List("me");
-                ListMessagesResponse response = request.Execute();
-                if (response.Messages != null && response.Messages.Count > 0)
+            ListMessagesResponse response = request.Execute();
+            if (response.Messages != null && response.Messages.Count > 0)
+            {
+                Console.WriteLine("Messages:");
+                foreach (var messageItem in response.Messages)
                 {
-                    Console.WriteLine("Messages:");
-                    foreach (var messageItem in response.Messages)
-                    {
-                        Console.WriteLine("- {0}", messageItem.Id);
-                    }
+                    Console.WriteLine("- {0}", messageItem.Id);
                 }
-                else
-                {
-                    Console.WriteLine("No new messages.");
-                }
-        */
+            }
+            else
+            {
+                Console.WriteLine("No new messages.");
+            }
+    */
         // I need code to get a list of email messages including From, Subject, Date from gmail in category CATEGORY_UPDATES
-
+        /*
         UsersResource.MessagesResource.ListRequest messageListRequest = service.Users.Messages.List("me");
         messageListRequest.Q = "in:updates";  // as opposed to label:xxx
         ListMessagesResponse messageListResponse = messageListRequest.Execute();
@@ -80,5 +81,7 @@ public class GmailAPI
         {
             Console.WriteLine("No messages found");
         }
+        */
+        
     }
 }
